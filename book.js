7da21fe2444e11d6cdb64b7cf04a1c3c821179d5
Kimala Cochran <kimalajoy@gmail.com>
2020-01-04 12:24:22 -0700

    completed exercise

--- book.js
+++ book.js
@@ -1,5 +1,9 @@
 class Book {
-
+  constructor (firstName, lastName, title, publicationYear) {
+    this.author = `${firstName} ${lastName}`;
+    this.title = title;
+    this.publicationYear = publicationYear;
+  }
 }
 
 module.exports = Book;
